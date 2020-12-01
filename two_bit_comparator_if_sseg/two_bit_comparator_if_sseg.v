f28470021096c896b79d4fd97e7933a4ae2ca8d4
psd0 <davissemi@gmail.com>
2020-11-30 20:23:13 -0500

    Initial commit

---
+++ two_bit_comparator_if_sseg/two_bit_comparator_if_sseg.v
@@ -0,0 +1,67 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: Davis Semiconductor
+// Engineer: psd0
+// 
+// Create Date:    15:55:11 11/30/2020 
+// Design Name: 
+// Module Name:    two_bit_comparator_if_sseg 
+// Project Name: 
+// Target Devices: Elbert V2
+// Tool versions: ISE 14.7
+// Description: Compares magnitude of 2 2-bit numbers a, b.  Returns a 1 if:
+//                 a > b
+//                 a < b
+//                 a = b
+//               The outputs are mutually exclusive.
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//  This is functionally equivalent to the 'two_bit_comparator' project, except
+//  this uses behavioural modelling.  As an added bonus, in addition to LED outputs,
+//  one of the 7-segment digits will indicate G, L or E for greater than, less than
+//  and equal to, respectively.
+//////////////////////////////////////////////////////////////////////////////////
+
+module two_bit_comparator_if(input [1:0] a, 
+                             input [1:0] b, 
+									  output reg a_gt_b, 
+									  output reg a_lt_b, 
+									  output reg a_eq_b,
+									  output reg [2:0] sseg_en,
+									  output reg [7:0] sseg);
+
+always @*
+  begin
+    a_gt_b = 1'b0;
+	 a_lt_b = 1'b0;
+	 a_eq_b = 1'b0;
+	 sseg_en = 3'b111; // disable all three 7-segment displays
+	 sseg = 8'b11111111; // turn off all segments
+	 
+	 if (a > b)
+	  begin
+	    a_gt_b = 1'b1;
+		 sseg_en = 3'b110; // 0 = 7-segment display is enabled
+		 sseg = 8'b01000011;
+	   end
+	 else if (a < b)
+	   begin
+	    a_lt_b = 1'b1;
+		 sseg_en = 3'b110; 
+		 sseg = 8'b11100011;
+		end
+	 else
+	    // a = b
+	  begin
+		 a_eq_b = 1'b1;
+		 sseg_en = 3'b110;
+		 sseg = 8'b01100001;
+	  end
+	end
+	
+endmodule
+
---
+++ two_bit_comparator_if_sseg/two_bit_comparator_sseg_tb.v
@@ -0,0 +1,100 @@
+`timescale 1ns / 1ps
+
+////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer:
+//
+// Create Date:   16:27:10 11/30/2020
+// Design Name:   two_bit_comparator_if
+// Module Name:   C:/Users/pauls/fpgadev/SPARTAN3/two_bit_comparator_if_sseg/two_bit_comparator_sseg_tb.v
+// Project Name:  two_bit_comparator_if_sseg
+// Target Device: Elbert V2
+// Tool versions: ISE 14.7
+// Description: 
+//
+// Verilog Test Fixture created by ISE for module: two_bit_comparator_if
+//
+// Dependencies:
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+////////////////////////////////////////////////////////////////////////////////
+
+module two_bit_comparator_sseg_tb;
+
+	// Inputs
+	reg [1:0] a;
+	reg [1:0] b;
+
+	// Outputs
+	wire a_gt_b;
+	wire a_lt_b;
+	wire a_eq_b;
+	wire sseg_en;
+	wire [7:0] sseg;
+
+	// Instantiate the Unit Under Test (UUT)
+	two_bit_comparator_if uut (
+		.a(a), 
+		.b(b), 
+		.a_gt_b(a_gt_b), 
+		.a_lt_b(a_lt_b), 
+		.a_eq_b(a_eq_b), 
+		.sseg_en(sseg_en), 
+		.sseg(sseg)
+	);
+
+	initial begin
+		// Initialize Inputs
+		a = 0;
+		b = 0;
+
+		// Wait 100 ns for global reset to finish
+		#100;
+        
+		// Add stimulus here
+
+	  // Add stimulus here
+      a = 2'b10; // a > b
+		b = 2'b01;
+		#200;
+		
+		a = 2'b11; // a > b
+		b = 2'b10;
+		#200;
+		
+		a = 2'b01; // a < b
+		b = 2'b10;
+		#200;
+		
+		a = 2'b11; // a = b 
+		b = 2'b11;
+		#200;
+		
+		a = 2'b10; // a = b
+		b = 2'b10;
+		#200;
+		
+		a = 2'b01; // a = b
+		b = 2'b01;
+		#200;
+		
+		a = 2'b00; // a = b
+		b = 2'b00; 
+		#200;
+		
+		a = 2'b01; // a < b
+		b = 2'b11; 
+		#200;
+		
+		$stop;
+	end
+   
+	initial begin
+	   $monitor("a = %b%b, b = %b%b, a_gt_b = %b, a_lt_b = %b, a_eq_b = %b, sseg_en = %d, sseg = %d", a[1], a[0], b[1], b[0], a_gt_b, a_lt_b, a_eq_b, sseg_en, sseg);
+	end
+      
+endmodule
+
